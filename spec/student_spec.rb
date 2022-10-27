9f9386023a1b9189619ae43a119989dd5375f957
Samuel Cox <samc1253@gmail.com>
2022-10-27 09:10:12 -0600

    Add test for name

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Student do
       
       expect(student).to be_a(Student)
     end
+
+    it 'has a name' do
+      student = Student.new({name: "Morgan", age: 21})    
+      
+      expect(student.name).to eq("Morgan")
+    end
   end
 end
