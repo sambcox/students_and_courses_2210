4f314bc76b4bed961e83022348ec2fc70769ba5d
Samuel Cox <samc1253@gmail.com>
2022-10-27 10:14:59 -0600

    Add test for finding students within a specific grade range

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -87,4 +87,37 @@ RSpec.describe Gradebook do
       expect(gradebook.students_grade_below(70)).to eq([student1, student2])
     end
   end
+
+  describe '#students_grade_in_range' do
+    it 'can list all students in the gradebook with a grade within a specific range' do
+      gradebook = Gradebook.new("Nathan")
+      course1 = Course.new("Calculus", 2)
+      course2 = Course.new("History", 4)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      student3 = Student.new({name: "Megan", age: 27})
+      student4 = Student.new({name: "Sierra", age: 27})
+      
+      student1.log_score(54)
+      student1.log_score(62)
+      student1.log_score(48)
+      student2.log_score(71)
+      student2.log_score(78)
+      student2.log_score(76)
+      student3.log_score(85)
+      student3.log_score(87)
+      student3.log_score(98)
+      student4.log_score(81)
+      student4.log_score(86)
+      student4.log_score(88)
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+      course1.enroll(student1)
+      course1.enroll(student2)
+      course2.enroll(student3)
+      course2.enroll(student4)
+
+      expect(gradebook.students_grade_in_range(70, 80)).to eq([student2])
+    end
+  end
 end
