53bd663053060536769978aa7500699eb84c5133
Samuel Cox <samc1253@gmail.com>
2022-10-27 10:00:51 -0600

    Add test for listing all students in gradebook

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -34,4 +34,24 @@ RSpec.describe Gradebook do
       expect(gradebook.courses).to eq(["Calculus", "History"])
     end
   end
+
+  describe '#all_students' do
+    it 'can list all students in the gradebook' do
+      gradebook = Gradebook.new("Nathan")
+      course1 = Course.new("Calculus", 2)
+      course2 = Course.new("History", 4)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      student3 = Student.new({name: "Megan", age: 27})
+      student4 = Student.new({name: "Sierra", age: 27})
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+      course1.enroll(student1)
+      course1.enroll(student2)
+      course2.enroll(student3)
+      course2.enroll(student4)
+
+      expect(gradebook.all_students).to eq([student1, student2, student3, student4])
+    end
+  end
 end
