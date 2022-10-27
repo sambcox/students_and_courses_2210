85d8c497ee2fd93fdc7f273ba297876a2678fa47
Samuel Cox <samc1253@gmail.com>
2022-10-27 10:07:47 -0600

    Add test for returning students in a gradebook that are below a certain grade

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -54,4 +54,37 @@ RSpec.describe Gradebook do
       expect(gradebook.all_students).to eq([student1, student2, student3, student4])
     end
   end
+
+  describe '#students_grade_below' do
+    it 'can list all students in the gradebook with a grade below a certain number' do
+      gradebook = Gradebook.new("Nathan")
+      course1 = Course.new("Calculus", 2)
+      course2 = Course.new("History", 4)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      student3 = Student.new({name: "Megan", age: 27})
+      student4 = Student.new({name: "Sierra", age: 27})
+      
+      student1.log_score(54)
+      student1.log_score(62)
+      student1.log_score(48)
+      student2.log_score(61)
+      student2.log_score(65)
+      student2.log_score(58)
+      student3.log_score(85)
+      student3.log_score(87)
+      student3.log_score(98)
+      student4.log_score(81)
+      student4.log_score(86)
+      student4.log_score(88)
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+      course1.enroll(student1)
+      course1.enroll(student2)
+      course2.enroll(student3)
+      course2.enroll(student4)
+
+      expect(gradebook.students_below(70)).to eq([student1, student2])
+    end
+  end
 end
