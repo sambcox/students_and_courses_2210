150b6c2e8b81f4c104979806a87136e8ff471002
Samuel Cox <samc1253@gmail.com>
2022-10-27 09:29:08 -0600

    Add tests for name and capacity

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -5,6 +5,20 @@ RSpec.describe Course do
   describe '#initialize' do
     it 'exists' do
       course = Course.new("Calculus", 2)
+
+      expect(course).to be_a(Course)
+    end
+
+    it 'has a name' do
+      course = Course.new("Calculus", 2)
+
+      expect(course.name).to eq("Calculus")
+    end
+
+    it 'has a capacity' do
+      course = Course.new("Calculus", 2)
+
+      expect(course.capacity).to eq(2)
     end
   end
 end
