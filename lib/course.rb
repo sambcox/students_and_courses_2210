45a7c101d91d521e7a0efc8d3c707bd6afabb113
Samuel Cox <samc1253@gmail.com>
2022-10-27 09:34:35 -0600

    Add method for full? and allow students to be read

--- lib/course.rb
+++ lib/course.rb
@@ -1,7 +1,12 @@
 class Course
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :students
   def initialize(course_name, capacity)
     @name = course_name
     @capacity = capacity
+    @students = []
+  end
+
+  def full?
+    students.length >= capacity
   end
 end
