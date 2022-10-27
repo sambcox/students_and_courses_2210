25978594d5ef07474a0b6cd936194ab9bc97c6cc
Samuel Cox <samc1253@gmail.com>
2022-10-27 09:03:49 -0600

    Add first test for student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,11 @@
 require './lib/student'
 
 RSpec.describe Student do
+  describe '#initialize' do
+    it 'exists' do
+      student = Student.new({name: "Morgan", age: 21})    
+      
+      expect(student).to be_a(Student)
+    end
+  end
 end
