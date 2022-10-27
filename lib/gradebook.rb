9a9c69d67ae879cb1f262c526c9208902ccb02de
Samuel Cox <samc1253@gmail.com>
2022-10-27 10:16:48 -0600

    Add method to find students within a specific grade range

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -20,4 +20,8 @@ class Gradebook
   def students_grade_below(grade_input)
     all_students.find_all { |student| student.grade < grade_input}
   end
+
+  def students_grade_in_range(grade_input_low, grade_input_high)
+    all_students.find_all { |student| student.grade < grade_input_high && student.grade > grade_input_low}
+  end
 end
