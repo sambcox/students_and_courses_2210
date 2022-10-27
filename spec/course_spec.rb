59b3ac19bfd3f5d475b6f0253f815d9ad9c015eb
Samuel Cox <samc1253@gmail.com>
2022-10-27 09:25:47 -0600

    Create course lib and spec files and add first test for course

---
+++ spec/course_spec.rb
@@ -0,0 +1,10 @@
+require './lib/student'
+require './lib/course'
+
+RSpec.describe Course do
+  describe '#initialize' do
+    it 'exists' do
+      course = course.new("Calculus", 2)
+    end
+  end
+end
